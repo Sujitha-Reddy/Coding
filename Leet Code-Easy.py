999f5e0814854d6259a257b6855817a82e1c5d7d
Sujitha-Reddy <60417272+Sujitha-Reddy@users.noreply.github.com>
2020-01-29 05:07:07 -0800

    Update Leet Code-Easy.py

--- Leet Code-Easy.py
+++ Leet Code-Easy.py
@@ -56,4 +56,41 @@ class Solution:
         if x<=0:
             x=-1*int(str(x*-1)[::-1])
         return x if x<=(2**31-1) and x>=-(2**31) else 0
- 
+#  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+# I can be placed before V (5) and X (10) to make 4 and 9. 
+# X can be placed before L (50) and C (100) to make 40 and 90. 
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000}
+        result=0
+        for i,j in enumerate(s):
+            if (i+1) == len(s) or roman_dict[j] >= roman_dict[s[i+1]]:
+                result += roman_dict[j]
+            else:
+                result -= roman_dict[j]
+        return result
+        
+        
+        
